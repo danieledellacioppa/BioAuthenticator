ab67b4173d2abcc7521be1e09559f893f5c3ec28
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-02-03 12:15:46 +0000

    dirty but works

--- app/src/main/java/com/forteur/bioauthenticator/MainActivity.kt
+++ app/src/main/java/com/forteur/bioauthenticator/MainActivity.kt
@@ -2,9 +2,11 @@ package com.forteur.bioauthenticator
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,9 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import com.forteur.bioauthenticator.ui.theme.BioAuthenticatorTheme
+import java.util.concurrent.Executor
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     var biometricManagerStatus : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +58,37 @@ class MainActivity : ComponentActivity() {
                 biometricManagerStatus = "Impossibile determinare lo stato della biometria."
             }
         }
+
+        // Configura l'autenticazione biometrica qui
+        val executor: Executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = androidx.biometric.BiometricPrompt(
+            this,
+            executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext, "Errore di autenticazione: $errString", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(applicationContext, "Autenticazione riuscita", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Autenticazione fallita", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Autenticazioneae Biometricae")
+            .setSubtitle("Accedie utilizzande le tue impronte digitale e se puoie ramme pure nu passagge")
+            .setNegativeButtonText("Annulla")
+            .build()
+
+        // Mostra il prompt biometrico all'avvio dell'Activity, o in base alla tua logica
+        biometricPrompt.authenticate(promptInfo)
 
         setContent {
             BioAuthenticatorTheme {
